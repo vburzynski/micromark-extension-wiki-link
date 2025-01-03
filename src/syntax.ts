24790972e087d4c4ffe8d09efbb49c058dc9a461
Valerie Burzynski <valerie@bendyworks.com>
2025-01-02 21:06:21 -0600

    refactor: decompose tokenization methods further

--- src/syntax.ts
+++ src/syntax.ts
@@ -15,12 +15,12 @@ import { asciiAlpha, markdownLineEnding, markdownSpace } from 'micromark-util-ch
 
 declare module 'micromark-util-types' {
   interface TokenTypeMap {
-    wikiLink: 'wikiLink',
-    wikiLinkMarker: 'wikiLinkMarker'
+    wikiLink: 'wikiLink', // encloses the entire wikilink
+    wikiLinkMarker: 'wikiLinkMarker' // encloses the opening or closing markers
     wikiLinkData: 'wikiLinkData'
-    wikiLinkTarget: 'wikiLinkTarget'
-    wikiLinkAliasMarker: 'wikiLinkAliasMarker'
-    wikiLinkAlias: 'wikiLinkAlias'
+    wikiLinkTarget: 'wikiLinkTarget' // encloses the first target value
+    wikiLinkAliasMarker: 'wikiLinkAliasMarker' // the alias marker that might appear after the target
+    wikiLinkAlias: 'wikiLinkAlias' // the alias value (appears after the alias marker)
   }
 }
 
@@ -32,7 +32,7 @@ function isLineEndingTabOrSpace(code: Code) {
   return code && (code < codes.nul || code === codes.space);
 }
 
-function isEndOfFile(code: number): boolean {
+function isEndOfFile(code: Code): boolean {
   return code === codes.eof;
 }
 
@@ -68,10 +68,10 @@ const tokenizeEscapedPipeAliasMarker: Tokenizer = function (
 const escapedVerticalBarAliasDivider: Construct = { tokenize: tokenizeEscapedPipeAliasMarker, partial: true };
 
 export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
-  const embedStartMarker = codes.exclamationMark;
-  const linkOpenMarker = '[[';
+  const embedMarker = codes.exclamationMark;
+  const openMarker = '[[';
   const aliasDivider = opts.aliasDivider ?? ':';
-  const linkCloseMarker = ']]';
+  const closeMarker = ']]';
 
   // when using a vertical bar as an alias divider, the wikilinks inside of GFM tables need to escape the vertical bar
   const employMarkdownTableFix = opts.aliasDivider === '|';
@@ -79,23 +79,23 @@ export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
   function tokenize(this: TokenizeContext, effects: Effects, ok: State, nok: State) {
     const self = this;
 
-    let data = false;
+    let containsTarget = false;
     let alias = false;
     let aliasCursor = 0;
-    let startMarkerCursor = 0;
+    let openMarkerSize = 0;
     let endMarkerCursor = 0;
 
     return start;
 
     function start(code: Code): State | undefined {
-      const firstCodeForLinkOpenMarker = linkOpenMarker.charCodeAt(0);
+      const firstCodeForLinkOpenMarker = openMarker.charCodeAt(0);
 
       // when the code matches the start marker
       if (code === firstCodeForLinkOpenMarker) {
         return startWikiLink(code);
       }
       // when the code matches the start embed marker
-      if (code === embedStartMarker) {
+      if (code === embedMarker) {
         return startEmbed(code);
       }
 
@@ -106,41 +106,47 @@ export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
       effects.enter('wikiLink');
       effects.enter('wikiLinkMarker');
 
-      return consumeStart(code);
+      return openingMarker(code);
     }
 
     function startEmbed(code: Code) {
       // when the code matches the start of an embed
-      if (code === embedStartMarker) {
+      if (code === embedMarker) {
         effects.enter('wikiLink', { embed: true });
         effects.enter('wikiLinkMarker', { embed: true });
         effects.consume(code);
 
-        return consumeStart;
+        return openingMarker;
       }
       return nok(code);
     }
 
-    function consumeStart(code: Code): State | undefined {
+    function openingMarker(code: Code): State | undefined {
       // when the code is the first character after the end of the starting marker
-      if (startMarkerCursor === linkOpenMarker.length) {
-        effects.exit('wikiLinkMarker');
-        return consumeData(code);
+      if (openMarkerSize === openMarker.length) {
+        return afterOpenMarker(code);
       }
 
       // when the code matches the starting marker
-      const nextStartMarkerCode = linkOpenMarker.charCodeAt(startMarkerCursor);
+      const nextStartMarkerCode = openMarker.charCodeAt(openMarkerSize);
       if (code === nextStartMarkerCode) {
-        startMarkerCursor++;
         effects.consume(code);
-        return consumeStart;
+        openMarkerSize++;
+
+        return openingMarker;
       }
 
       return nok(code);
     }
 
+    function afterOpenMarker(code: Code): State | undefined {
+      effects.exit('wikiLinkMarker');
+
+      return consumeData(code);
+    }
+
     function consumeData(code: Code): State | undefined {
-      if (!code || markdownLineEnding(code) || isEndOfFile(code)) {
+      if (markdownLineEnding(code) || isEndOfFile(code)) {
         return nok(code);
       }
 
@@ -158,36 +164,49 @@ export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
       // when the code matches the alias marker
       const nextAliasMarkerCode = aliasDivider.charCodeAt(aliasCursor);
       if (code === nextAliasMarkerCode) {
-        // when there is no title component inside the wikilink data, this is invalid syntax
-        if (!data) return nok(code);
-        effects.exit('wikiLinkTarget');
-        effects.enter('wikiLinkAliasMarker');
-        return consumeAliasMarker(code);
+        return beforeAliasMarker(code);
       }
 
       // when the code matches the end marker
-      const nextEndMarkerCode = linkCloseMarker.charCodeAt(endMarkerCursor);
+      const nextEndMarkerCode = closeMarker.charCodeAt(endMarkerCursor);
       if (code === nextEndMarkerCode) {
-        // when there is no title component inside the wikilink data, this is invalid syntax
-        if (!data) return nok(code);
-        effects.exit('wikiLinkTarget');
-        effects.exit('wikiLinkData');
-        effects.enter('wikiLinkMarker');
-        return consumeEnd(code);
+        return beforeCloseMarker(code);
       }
 
-      if (!code || markdownLineEnding(code) || isEndOfFile(code)) {
+      if (markdownLineEnding(code) || isEndOfFile(code)) {
         return nok(code);
       }
 
       // when the code matches non-whitespace, we know data exists inside the wikilink
       if (!isLineEndingTabOrSpace(code)) {
-        data = true;
+        containsTarget = true;
       }
 
       effects.consume(code);
 
       return consumeTarget;
+    }
+
+    function beforeAliasMarker(code: Code): State | undefined {
+      if (containsTarget) {
+        effects.exit('wikiLinkTarget');
+        effects.enter('wikiLinkAliasMarker');
+        return consumeAliasMarker(code);
+      }
+
+      return nok(code);
+    }
+
+    function beforeCloseMarker(code: Code): State | undefined {
+      if (containsTarget) {
+        effects.exit('wikiLinkTarget');
+        effects.exit('wikiLinkData');
+        effects.enter('wikiLinkMarker');
+
+        return consumeEnd(code);
+      }
+
+      return nok(code);
     }
 
     function consumeAliasMarker(code: Code): State | undefined {
@@ -212,7 +231,7 @@ export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
 
     function consumeAlias(code: Code): State | undefined {
       // when the code matches the end marker
-      const nextEndMarkerCode = linkCloseMarker.charCodeAt(endMarkerCursor);
+      const nextEndMarkerCode = closeMarker.charCodeAt(endMarkerCursor);
       if (code === nextEndMarkerCode) {
         // when we reach the end marker without seeing an alias, abort
         if (!alias) return nok(code);
@@ -222,7 +241,7 @@ export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
         return consumeEnd(code);
       }
 
-      if (!code || markdownLineEnding(code) || isEndOfFile(code)) {
+      if (markdownLineEnding(code) || isEndOfFile(code)) {
         return nok(code);
       }
 
@@ -239,14 +258,14 @@ export function syntax(opts: WikiLinkSyntaxOptions = {}): Extension {
     function consumeEnd(code: Code): State | undefined {
       debugger
       // the syntax is valid and complete after the entire length of the end marker is matched
-      if (endMarkerCursor === linkCloseMarker.length) {
+      if (endMarkerCursor === closeMarker.length) {
         effects.exit('wikiLinkMarker');
         effects.exit('wikiLink');
         return ok(code);
       }
 
       // when the code matches th next character in the end marker
-      const nextEndMarkerCode = linkCloseMarker.charCodeAt(endMarkerCursor);
+      const nextEndMarkerCode = closeMarker.charCodeAt(endMarkerCursor);
       if (code !== nextEndMarkerCode) {
         return nok(code);
       }
